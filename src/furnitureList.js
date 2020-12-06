7cd982fe70a7e27230537e6025539b9ae364ffc7
SaiPradeepti <saipradeepti14@gmail.com>
2020-12-06 14:28:42 +0530

    Adding furnitureList.js

---
+++ src/furnitureList.js
@@ -0,0 +1,52 @@
+const furnitureList = [
+    {       
+        id: 1,
+        title: 'orange sofa',
+        image: 'https://images.unsplash.com/photo-1550226891-ef816aed4a98?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8ZnVybml0dXJlfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        price: 100,
+    },
+    {   
+        id: 2,
+        title: 'blue chair',
+        image: 'https://images.unsplash.com/photo-1541558869434-2840d308329a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fGZ1cm5pdHVyZXxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        price: 80,
+    },
+    {   
+        id: 3,
+        title: 'grey sofa',
+        image: 'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8ZnVybml0dXJlfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        price: 120,
+    },
+    {   
+        id: 4,
+        title: 'outdoor dining table',
+        image: 'https://images.unsplash.com/photo-1537182534312-f945134cce34?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHRhYmxlfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        price: 250,
+    },
+    {   
+        id: 5,
+        title: 'multipurpose table',
+        image: 'https://images.unsplash.com/photo-1590693058087-0aef5a71ed67?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzl8fHRhYmxlfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        price: 170,
+    },
+    {   
+        id: 6,
+        title: 'table chair set',
+        image: 'https://images.unsplash.com/photo-1587258459922-4521d3704511?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzR8fHRhYmxlfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        price: 170,
+    },
+    {   
+        id: 7,
+        title: 'black sofa',
+        image: 'https://images.unsplash.com/photo-1559599224-f1d3a48f1244?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OTF8fHRhYmxlfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        price: 200,
+    },
+    {   
+        id: 8,
+        title: 'outdoor swing black',
+        image: 'https://images.unsplash.com/photo-1585298108138-694c7200931a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8b3V0ZG9vciUyMHN3aW5nfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        price: 300,
+    },
+];
+
+export default furnitureList;
