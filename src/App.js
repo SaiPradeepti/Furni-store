a29d09a398b1185d59c794522d73946d3544a63f
SaiPradeepti <saipradeepti14@gmail.com>
2020-12-06 14:30:52 +0530

    Updating App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-
+import furnitureList from './furnitureList'
+import Furniture from './Furniture'
 
 function App() {  
-  
+  return (
+    <div className="App">
+      {
+        furnitureList.map(item => {
+          return <Furniture key={item.id} {...item} />
+        })
+      }
+    </div>
+  );
 }
 
 export default App;
